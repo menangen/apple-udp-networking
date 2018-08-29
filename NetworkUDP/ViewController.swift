44fcb2af445d8fe5ba5e35ae38d36b2849ab08e8
Andrey Menangen <menangen@gmail.com>
2018-08-29 10:45:25 +0300

    Initial Commit

---
+++ NetworkUDP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkUDP
+//
+//  Created by menangen on 29.08.2018.
+//  Copyright © 2018 menangen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
