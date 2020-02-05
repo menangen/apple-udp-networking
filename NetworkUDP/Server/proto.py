6d143de03cf84509f4b406b0904c69abf2343883
Andrey Menangen <menangen@gmail.com>
2020-02-05 13:33:18 +0300

    Chunk IDs

--- NetworkUDP/Server/proto.py
+++ NetworkUDP/Server/proto.py
@@ -5,15 +5,25 @@ class Protocol:
 
     def __init__(self, version: int = 1):
         self.VERSION = version
+        self.CHUNK = 1
+        self.CHUNKS_NUMBER = 1
+
+    @staticmethod
+    def get_chunk_id(current: int = 0, total: int = 0):
+        return current << 4 | total
 
     def get_header(self, data_length: int):
-        return bytes([self.VERSION, data_length])
+        chunk_id = self.get_chunk_id(self.CHUNK, self.CHUNKS_NUMBER)
+
+        return bytes([self.VERSION, chunk_id, data_length])
+
+    def encode(self, data: bytes, size: int):
+        header_data = self.get_header(size)
+
+        return header_data + data
 
     def for_string(self, text: str):
-        header_data = self.get_header(len(text))
-
-        return header_data + text.encode()
+        return self.encode(text.encode(), len(text))
 
     def for_integer(self, var: int, size: int = 1):
-        header_data = self.get_header(size)
-        return header_data + NetworkData.to_bytes(size, var)
+        return self.encode(NetworkData.to_bytes(size, var), size)
