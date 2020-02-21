0a7ef352a2397903dfbed322dadd17ea2e0f9c60
Andrey Menangen <menangen@gmail.com>
2020-02-21 19:23:04 +0300

    DNS service multiple A records

--- dns-resolver/dns.swift
+++ dns-resolver/dns.swift
@@ -9,36 +9,38 @@
 import Foundation
 import dnssd
 
-func resolveDNS() {/*
-    let group = DispatchGroup()
+let group = DispatchGroup()
+
+let callback: DNSServiceQueryRecordReply = {
+    (sdRef, flags, interfaceIndex, errorCode, fullname, rrtype, rrclass, rdlen, rawData, ttl, context) -> Void in
+    print(ttl, rdlen)
     
-    let callback: DNSServiceQueryRecordReply = {
-        (sdRef, flags, interfaceIndex, errorCode, fullname, rrtype, rrclass, rdlen, rawData, ttl, context) -> Void in
-        /*
-         let data = Data(bytes: rawData!, count: Int(rdlen))
-         let str = String(data: data, encoding: String.Encoding.ascii)
-         */
-        
-        /*
-         let uintPointer = UnsafePointer<UInt8>()
-         let str = UInt8(uintPointer)
-         */
-        let memory = rawData!.bindMemory(to: UInt8.self, capacity: 4)
-        
-        print(memory[0], memory[1], memory[2], memory[3])
-        print(ttl, Int(rdlen))
-        
-        group.leave()
-    }
+    /*
+     let data = Data(bytes: rawData!, count: Int(rdlen))
+     let str = String(data: data, encoding: String.Encoding.ascii)
+     */
+    
+    /*
+     let uintPointer = UnsafePointer<UInt8>()
+     let str = UInt8(uintPointer)
+     */
+    let memory = rawData!.bindMemory(to: UInt8.self, capacity: 4)
+    
+    print(memory[0], memory[1], memory[2], memory[3])
+    
+    //group.leave()
+}
+
+func resolveDNS() {
     
     let concurrentQueue = DispatchQueue(label: "queuename", attributes: .concurrent)
     
     var service: DNSServiceRef?
     let domainName = "vienna.novikova.us"
     
-    let error = DNSServiceQueryRecord(
+    DNSServiceQueryRecord(
         &service,
-        kDNSServiceFlagsTimeout,
+        kDNSServiceFlagsShared | kDNSServiceFlagsTimeout,
         0,
         domainName,
         UInt16(kDNSServiceType_A),
@@ -50,30 +52,28 @@ func resolveDNS() {/*
     
     let fd = DNSServiceRefSockFD(service)
     print("Socket created", fd)
+    
     let eventSource = DispatchSource.makeReadSource(fileDescriptor: fd, queue: concurrentQueue)
     
-    
     eventSource.setEventHandler {
-        print("Event...")
         
-        print("DNSServiceProcessResult")
+        print("DNSServiceProcessResult...")
         let res = DNSServiceProcessResult(service)
         
         if (res != kDNSServiceErr_NoError) { print("There was an error reading the DNS record") }
-        
+        group.leave()
     }
     eventSource.setCancelHandler {
         print("Cancel")
-        
+        DNSServiceRefDeallocate(service)
         group.leave()
     }
     
-    concurrentQueue.asyncAfter(wallDeadline: .now() + 2) { eventSource.cancel() }
+    concurrentQueue.asyncAfter(wallDeadline: .now() + 1) { eventSource.cancel() }
     
     group.enter()
     eventSource.resume();
     group.wait()
     
-    DNSServiceRefDeallocate(service)*/
     print("Hello, DNS!")
 }
